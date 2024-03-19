0996296b928e64815288c796aabba67ab96b8154
Service Web <sw@gssmail.com>
2024-03-19 09:26:32 -0500

    Commit changes in C:\Websites\Git\emarlow\6544\GCG_6544_SO_INCIDENT.sql

---
+++ GCG_6544_SO_INCIDENT.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "GCG_6544_SO_INCIDENT" (
+ "Order_no" CHAR(7),
+ "Incident_Type" CHAR(50),
+ "Incident" CHAR(50),
+ "Notes" LONGVARCHAR,
+ "Serial_No" CHAR(20),
+ "Incident_ID" NUMERIC(8,0) );
